6912d87e8037409eb0ab43e75cc9e9b5eee9e015
Yash <yashyk9891@gmail.com>
2025-07-24 16:23:15 +0530

    created redirect logic

--- src/main/java/com/url/shortener/service/UrlMappingService.java
+++ src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -90,4 +90,9 @@ public class UrlMappingService {
         return clickEvents.stream()
                 .collect(Collectors.groupingBy(click->click.getClickDate().toLocalDate(), Collectors.counting()));
     }
+
+    public UrlMapping getOriginalUrl(String shortUrl){
+        UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+        return urlMapping;
+    }
 }
