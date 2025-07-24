3b1889f0e82756645c42c0fb2392f23893ee7af4
Yash <yashyk9891@gmail.com>
2025-07-24 16:29:08 +0530

    created clickEvent Analytics count logic

--- src/main/java/com/url/shortener/service/UrlMappingService.java
+++ src/main/java/com/url/shortener/service/UrlMappingService.java
@@ -93,6 +93,18 @@ public class UrlMappingService {
 
     public UrlMapping getOriginalUrl(String shortUrl){
         UrlMapping urlMapping = urlMappingRepository.findByShortUrl(shortUrl);
+
+        if(urlMapping!=null){
+            urlMapping.setClickCount(urlMapping.getClickCount()+1);
+            urlMappingRepository.save(urlMapping);
+
+            //Record clickEvent
+            ClickEvent clickEvent = new ClickEvent();
+            clickEvent.setClickDate(LocalDateTime.now());
+            clickEvent.setUrlMapping(urlMapping);
+            clickEventRepository.save(clickEvent);
+        }
+
         return urlMapping;
     }
 }
